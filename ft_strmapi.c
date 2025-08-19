71d7650956aa5aabc7abb19dd38c681b4d34b3dd
Mohammad AlHindi <94603043+mohammadalhindi1@users.noreply.github.com>
2025-08-19 04:36:58 +0300

    Add files via upload

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,18 +1,60 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: malhendi <malhendi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/18 17:17:56 by malhendi          #+#    #+#             */
-/*   Updated: 2025/08/18 20:21:29 by malhendi         ###   ########.fr       */
+/*   Created: 2025/08/18 18:09:41 by malhendi          #+#    #+#             */
+/*   Updated: 2025/08/18 20:21:26 by malhendi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    
+	unsigned int	i;
+	unsigned int	len;
+	char			*res;
+
+	if (!s || f == NULL)
+		return NULL;
+
+	len = ft_strlen(s);
+
+	res = (char *)malloc((len + 1) * sizeof(char));
+	if (res == NULL)
+		return NULL;
+
+	i = 0;
+	while (i < len)
+	{
+		res[i] = f(i, s[i]); //res[i] = (*f)(i, s[i]);
+		i++;
+	}
+	res[i] = '\0';
+
+	return (res);
 }
+
+/*
+int main(void)
+{
+    char *str = "mohammad alhendi";
+    char *result;
+
+    result = ft_strmapi(str, (char (*)(unsigned int, char))ft_toupper);
+
+    if (result)
+    {
+        printf("Original: %s\n", str);
+        printf("Mapped  : %s\n", result);
+        free(result);
+    }
+    else
+        printf("Memory allocation failed!\n");
+
+    return 0;
+}
+*/
